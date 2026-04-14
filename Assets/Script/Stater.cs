f6747cca55ddaa41b7e9254b2648d25b00942d32
elliot.hult <elliot.hult@elev.ga.ntig.se>
2026-04-14 16:03:06 +0200

    Marken funkar nu

---
+++ Assets/Script/Stater.cs
@@ -0,0 +1,35 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Stater : MonoBehaviour
+{
+    Animator a;
+    fps f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        a=GetComponent<Animator>();
+        f=GetComponentInParent<fps>();
+    }
+
+    // Update is called once per frame
+    float oldvault=0;
+    void Update()
+    {
+        a.SetFloat("Speed", f.speed/f.maxspeed);
+        if (f.speed==0)
+        {
+            a.SetFloat("Speed", -1);
+        }
+        
+        
+        if (f.vault > oldvault)
+        {
+            a.SetTrigger("Vault"); 
+        }
+        a.SetBool("Jumping", f.j && f.grounded);
+        oldvault = f.vault;
+        a.SetBool("Falling", !f.grounded && !a.GetBool("Jumping"));
+
+    }
+}
--- Assets/Script/fps.cs
+++ Assets/Script/fps.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Security.Cryptography;
 using Unity.Mathematics;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
@@ -8,21 +10,24 @@ using UnityEngine.Rendering;
 
 public class fps : MonoBehaviour
 {
-    [SerializeField] float sens = 150;
-    [SerializeField] float accel = 4;
-    [SerializeField] float maxspeed = 20;
-    [SerializeField] float jumpy = 10;
-    [SerializeField] float MinJumpyM = 0.5f;
-    [SerializeField] float MaxJumpyM = 1.5f;
-    [SerializeField] float gravM = 2;
-    float speed = 0;
+    [SerializeField] public float sens = 150;
+    [SerializeField] public float accel = 4;
+    [SerializeField] public float maxspeed = 20;
+    [SerializeField] public float jumpy = 10;
+    [SerializeField] public float MinJumpyM = 0.5f;
+    [SerializeField] public float MaxJumpyM = 1.5f;
+    [SerializeField] public float gravM = 2;
+    public float speed = 0;
+    public float vault = 0;
+    public bool grounded = false;
     float smallening = 0.7f;
     float slowening = 0.7f;
+    GameObject h; // vault obj
     Camera c;
     Vector2 inp;
     Vector2 look;
     CharacterController cc;
-    Boolean j=false;
+    public Boolean j=false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -33,25 +38,44 @@ public class fps : MonoBehaviour
 
     // Update is called once per frame
     Vector3 vel=Vector3.zero;
-    void Update()
+    bool IsGrounded()
     {
+        if (Physics.Raycast(transform.position, new(0,-1,0),out RaycastHit k))
+        {   print(k.distance-(transform.localScale.y*cc.height/2)+cc.skinWidth);
+        // -new Vector3(0,(transform.localScale.y*cc.height/2)+cc.skinWidth,0)
+            if (k.distance-(transform.localScale.y*cc.height/2)+cc.skinWidth < .2f)
+            {
+                k.collider.gameObject.GetComponent<Renderer>().material.color = UnityEngine.Random.ColorHSV(); 
+                //cc.Move(new(0,-k.distance,0)); //-k.distance+.02f
+                return true;
+            }
+        } else {print("void?");}
+        return false;
+    }
+    void FixedUpdate()
+    {
+        print("ground: ");
+        grounded = IsGrounded();
         vel.y+=gravM*Physics.gravity.y*Time.deltaTime;
-        
+        if (vault != 0)
+        {
+            vault-=Time.deltaTime;
+            if (vault <= 0) {h.GetComponent<Collider>().enabled = true;vault=0;}
+        }
         if (speed < maxspeed) {speed += accel*inp.magnitude*Time.deltaTime;} else {speed = maxspeed;}
         if (inp.magnitude < 0.2 && speed>0) {speed -= 30*Time.deltaTime;if(speed<0){speed=0;}}
         vel.z = speed;
-        if (cc.isGrounded) //on ground
+        if (grounded) //on ground
         {
             if (j) 
             {
-                
                 if (transform.localScale.y < 1)
                 {
                     vel.y = jumpy*moveM()/2;
                     vel.z += jumpy;
                 } else {vel.y=jumpy*moveM();}
             } 
-            else {vel.y=-0.1f;}
+            else {vel.y=0;}
             
         } else //in air
         {if (vel.y>0&&!j) {vel.y-=20*Time.deltaTime;}}
@@ -70,7 +94,7 @@ public class fps : MonoBehaviour
         };
     void AttemptCrouch()
     {
-        float d = (cc.height+cc.radius)*transform.localScale.y;
+        float d = cc.height*transform.localScale.y;
         r.origin = transform.position+new Vector3(0,d,0);
         cc.Raycast(r, out RaycastHit i, d-0.1f);
         if (transform.localScale.y < 1){transform.localScale = Vector3.one;maxspeed/=slowening;}
@@ -90,17 +114,17 @@ public class fps : MonoBehaviour
         j=!j;
         if (j)
         {
-           Boolean w = Physics.SphereCast(transform.position, cc.radius, transform.TransformDirection(new(0,0,1)), out RaycastHit h, 2);
+           Boolean w = Physics.SphereCast(transform.position, cc.radius, transform.TransformDirection(new(0,0,1)), out RaycastHit rh, 2);
             if (w)
             {
-                if (!Physics.Raycast(transform.position+new Vector3(0,2,0), transform.TransformDirection(new(0,0,1)), 2))
+                if (!Physics.Raycast(transform.position+new Vector3(0,2,0), transform.TransformDirection(new(0,0,1)), rh.distance))
                 {
                     print("vault");
-                    h.collider.enabled = false;
+                    h=rh.collider.gameObject;
+                    rh.collider.enabled = false;
                     vel.z = speed+vel.y;
                     vel.y = 10;
-                    new WaitForSeconds(0.5f);
-                    h.collider.enabled = true;
+                    vault = 1;
                     
                 }
                 
